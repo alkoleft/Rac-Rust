e02d6c109151e25a40ee39ad399f64a8915dd395
alkoleft <alkoleft@gmail.com>
2026-02-28 02:45:02 +0300

    clear

--- apps/rac_protocol/src/lib.rs
+++ apps/rac_protocol/src/lib.rs
@@ -2,7 +2,6 @@ pub mod client;
 pub mod codec;
 pub mod commands;
 pub mod error;
-pub mod metadata;
 pub mod protocol;
 pub mod rac_wire;
 pub mod rpc;
--- apps/rac_protocol/src/metadata.rs
+++
@@ -1,7 +0,0 @@
-use crate::rpc::{Meta, Request};
-
-pub type RpcMethodMeta = Meta;
-
-pub fn rpc_meta<R: Request>(request: &R) -> Meta {
-    request.meta()
-}
